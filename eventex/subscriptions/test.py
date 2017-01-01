d0313338764b9e1bf754c7e140ca8b62bc5899ae
Paulo César <paulocesarcs.dev@gmail.com>
2017-01-01 21:30:42 -0200

    Adicionado TDD da template do formulario de inscricao, adicionado views do formulario url do formulario

---
+++ eventex/subscriptions/test.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+class SubscribeTest(TestCase):
+    def test_get(self):
+        """Get /inscrição/ must return status code"""
+        response = self.client.get('/inscricao/')
+        self.assertEqual(200, response.status_code)
+
+    def test_template(self):
+        """Must use subscriptions/subscription_form.html"""
+        response = self.client.get('/inscricao/')
+        self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
--- eventex/urls.py
+++ eventex/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from eventex.core import views
 #from eventex.core views import home
+from eventex.subscriptions.views import subscribe
+
 urlpatterns = [
     #url(r'^$', home),
+    url(r'^inscricao/$', subscribe),
     url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
