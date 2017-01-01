f034e24605fc7726f44e5fa83c383eb893df0365
Paulo César <paulocesarcs.dev@gmail.com>
2017-01-01 21:59:38 -0200

    adicionado o formulario funcional com seu teste e csrf_token

--- eventex/subscriptions/test.py
+++ eventex/subscriptions/test.py
@@ -1,12 +1,24 @@
 from django.test import TestCase
 
 class SubscribeTest(TestCase):
+    def test_csrf(self):
+        """Html must contain csrf"""
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
     def test_get(self):
         """Get /inscrição/ must return status code"""
-        response = self.client.get('/inscricao/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
         """Must use subscriptions/subscription_form.html"""
-        response = self.client.get('/inscricao/')
-        self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+
+    def test_html(self):
+        """Html must contain input tags"""
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, 'type="email"')
+        self.assertContains(self.resp, 'type="submit"')
