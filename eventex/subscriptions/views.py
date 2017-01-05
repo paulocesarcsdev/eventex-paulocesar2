22090fc0f8e835da8d819f5a6b406d0cb03cfc00
Paulo César <paulocesarcs.dev@gmail.com>
2017-01-05 00:02:59 -0200

    Adicionado txt com a mensagem para teste, esta funcionado

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,13 +1,22 @@
 from django.core import mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
     if request.method == 'POST':
+
+        context = dict(name='Paulo Cesar', cpf='12345678901',
+                       email='paulo@cesar.net', phone='62-9413-0086')
+
+        body = render_to_string('subscriptions/subscription_email.txt',
+                                context)
+
         mail.send_mail('Confirmação da inscrição',
-                        MESSAGE,
+                        body,
                        'contato@eventex.com.br',
                        ['contato@eventex.com.br', 'paulo@cesar.net'])
 
@@ -16,25 +25,3 @@ def subscribe(request):
         context = {'form': SubscriptionForm()}
         return render(request, 'subscriptions/subscription_form.html', context)
 
-
-MESSAGE = """
-Olá Tudo bem?
-
-Muito obrigado por se inscrever no Eventex.
-
-Estes foram os dados que você nos forneceu
-em sua inscrição:
-
-Nome: Paulo Cesar
-CPF: 12345678901
-Email: paulo@cesar.net
-Telefone: 62-9413-0086
-
-Em até 48 horas úteis, a nossa equipe entrará
-em contato com você para concluirmos a sua
-matrícula.
-
-Antenciosamente,
---
-Morena
-"""
