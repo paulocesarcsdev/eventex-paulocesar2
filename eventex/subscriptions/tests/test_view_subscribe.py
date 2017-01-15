ad977f1ac527d1a9fa270ebc9b8fdabfedf5fb0a
Paulo César <paulocesarcs.dev@gmail.com>
2017-01-15 19:10:04 -0200

    Persiste a inscrição

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,12 +1,14 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 class SubscribeGet(TestCase):
     def test_csrf(self):
         """Html must contain csrf"""
         self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
     def setUp(self):
         self.resp = self.client.get('/inscricao/')
 
@@ -49,6 +51,9 @@ class SubscribePostValid(TestCase):
     def test_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
 
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
+
 
 class SubscribeInvalid(TestCase):
     def setUp(self):
@@ -68,6 +73,9 @@ class SubscribeInvalid(TestCase):
     def test_form_has_erros(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSuccessMessage(TestCase):
